f2228c29e4c682655dfdef9f32edd0c43477f540
ramy <52378913+7-figures@users.noreply.github.com>
2023-08-10 22:23:39 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from  './Components/Navbar';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+function App() {
+  return (
+    
+       <div className="container">
+            <Navbar />
+            <Main />
+            <Footer />
+        </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react"
+ export default function Navbar() {
+    return (
+        <nav>
+            <img src="./ramy.jpg" alt="me" className="head-icon"  />
+        </nav>
+    )
+}
