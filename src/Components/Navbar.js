7a72f91264423fdd81c6d9d357b724f2fa9f2656
ramy <52378913+7-figures@users.noreply.github.com>
2023-08-10 15:37:04 -0700

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react"
  export default function Navbar() {
     return (
         <nav>
-            <img src="./ramy.jpg" alt="me" className="head-icon"  />
+            <img src="./ramy.jpg"  className="head-icon"  />
         </nav>
     )
 }
